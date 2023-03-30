d882496b377cca7a55f5b9c19226f5d37c67774a
NicolasNazarRugs <126761915+NicolasNazarRugs@users.noreply.github.com>
2023-03-30 16:28:18 +0200

    Update app.php
    Modification pour la mise en production

--- config/app.php
+++ config/app.php
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', true),
+    'debug' => (bool) env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://edi.nazarrugs.fr'),
 
     'asset_url' => env('ASSET_URL'),
 
