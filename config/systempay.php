9a84bb2874ad2f5cb7967ab73b4283b30f0c595e
NicolasNazarRugs <126761915+NicolasNazarRugs@users.noreply.github.com>
2023-03-30 16:31:27 +0200

    Update systempay.php
    Modification pour mise en production

--- config/systempay.php
+++ config/systempay.php
@@ -3,8 +3,8 @@
 return [
     'default' => [
         'site_id' => '96428646',
-        'key'     => env('SYSTEMPAY_SITE_KEY', 'G5wZJJkAPukJJ5LB'),
-        'env'     => env('SYSTEMPAY_ENV', 'TEST'),
+        'key'     => env('SYSTEMPAY_SITE_KEY', 'uEAtsaemHrlllkvF'),
+        'env'     => env('SYSTEMPAY_ENV', 'PRODUCTION'),
         'currency' => '978',
         'payment_config' => 'SINGLE',
     ],
