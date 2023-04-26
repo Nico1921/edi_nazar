adc546100b65fdd54c40fc3d90d8fbef49651e52
Nicolas Bertrand <nbertrandnazar@gmail.com>
2023-04-26 11:51:24 +0200

    Résolution conflit deux fonction avec le meme nom

--- app/Models/Produit.php
+++ app/Models/Produit.php
@@ -256,9 +256,4 @@ class Produit extends Model
 
         return $prixProduit;
     }
-
-    public function gamme()
-    {
-        return $this->hasOne(Gamme::class,'id_gamme','gamme_id');
-    }
 }
