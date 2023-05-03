b6149c13662a30b635a87fa3bd26302f38768872
Nicolas Bertrand <nbertrandnazar@gmail.com>
2023-05-03 14:41:22 +0200

    Ajout du trie sur view_product

--- app/Http/Controllers/DropshippingController.php
+++ app/Http/Controllers/DropshippingController.php
@@ -353,8 +353,8 @@ class DropshippingController extends Controller
              ->where('produit.drop_shipping', '=', '1')
              ->where('produit.statut', '=', '1')
              ->where('id_design', '=', $designpanier[$i]->id_design)
-             ->orderBy('dimension.longueur', 'asc')
              ->orderBy('dimension.largeur', 'asc')
+             ->orderBy('dimension.longueur', 'asc')
              ->get();
              for ($j = 0; $j < count($produit); $j++) {
                  $design->$i->produits->$j = new \stdClass;
@@ -399,8 +399,8 @@ class DropshippingController extends Controller
              ->where('produit.drop_shipping', '=', '1')
              ->where('produit.statut', '=', '1')
              ->where('id_design', '=', $designpanier[$i]->id_design)
-             ->orderBy('dimension.longueur', 'asc')
              ->orderBy('dimension.largeur', 'asc')
+             ->orderBy('dimension.longueur', 'asc')
              ->get();
              for ($j = 0; $j < count($produit); $j++) {
                  $design->$i->produits->$j = new \stdClass;
